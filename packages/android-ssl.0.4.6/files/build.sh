611b57dc748bebcba54e9bcea12063c415a05bb9
Jerome Vouillon <jerome.vouillon@pps.univ-paris-diderot.fr>
2013-01-17 15:53:47 +0100

    Package android-ssl: fix library order

--- packages/android-ssl.0.4.6/files/build.sh
+++ packages/android-ssl.0.4.6/files/build.sh
@@ -6,7 +6,7 @@ EXEC_PREFIX=arm-linux-androideabi-
 TOOLCHAIN=${ANDROID_NDK}/toolchains/${EXEC_PREFIX}4.7/prebuilt/linux-x86/bin
 SYSROOT=$PREFIX/lib/android-ndk-linux/platforms/android-14/arch-arm
 INCLUDE=$PREFIX/arm-linux-androideabi/include
-CC="$TOOLCHAIN/${EXEC_PREFIX}gcc --sysroot $SYSROOT -I $INCLUDE"
+#CC="$TOOLCHAIN/${EXEC_PREFIX}gcc --sysroot $SYSROOT -I $INCLUDE"
 
 CPPFLAGS="--sysroot $SYSROOT -I$PREFIX/arm-linux-androideabi/include"
 LDFLAGS=-L$PREFIX/arm-linux-androideabi/lib
@@ -16,5 +16,5 @@ export CPPFLAGS LDFLAGS PATH
 
 ./configure --host arm-linux-androideabi OCAMLFIND="$PREFIX/bin/ocamlfind -toolchain android" LIBS=-lcrypto
 
-make
+make CLIBS="ssl crypto"
 make install
